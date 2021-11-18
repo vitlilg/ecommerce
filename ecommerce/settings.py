1277d4f03966f4d459c8b9a715c6ae7557778992
Vitaliy Grachev <vitlilg@gmail.com>
2021-11-18 12:07:12 +0200

    Full project with local db

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommercedb',
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '192.168.1.14',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,3 +1,4 @@
+
 """ecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
